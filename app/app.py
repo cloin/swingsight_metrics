f622c716538bb847b409ba6519da555d1f4df616
Colin McNaughton <c.a.mcnaughton@gmail.com>
2023-08-25 21:10:24 +0000

    better app

--- app/app.py
+++ app/app.py
@@ -17,26 +17,21 @@ latest_metrics = {
 @app.route('/golfball-hit', methods=['POST'])
 def golfball_hit():
     try:
-        # Extract JSON data from the request
         data = request.json
         
-        # Extract values from the JSON data
-        angle_of_departure = data['angle_of_departure']
-        velocity_at_departure = data['velocity_at_departure']
+        angle = data['angle_of_departure']
+        velocity = data['velocity_at_departure']
         
-        # Calculate projected yardage and zone
-        projected_yardage = calculate_projected_yardage(angle_of_departure, velocity_at_departure)
-        projected_zone = calculate_projected_zone(projected_yardage)
+        yardage = calculate_projected_yardage(angle, velocity)
+        zone = calculate_projected_zone(yardage)
         
-        # Update the latest metric values
-        latest_metrics["angle_of_departure"] = angle_of_departure
-        latest_metrics["velocity_at_departure"] = velocity_at_departure
-        latest_metrics["projected_yardage"] = projected_yardage
-        latest_metrics["projected_zone"] = projected_zone
-        latest_metrics["total_balls_hit"] += 1  # Increment the total balls hit metric
+        latest_metrics["angle_of_departure"] = angle
+        latest_metrics["velocity_at_departure"] = velocity
+        latest_metrics["projected_yardage"] = yardage
+        latest_metrics["projected_zone"] = zone
+        latest_metrics["total_balls_hit"] += 1
 
-        # Increment the count for the respective zone
-        zone_metric_name = f"count_zone_{projected_zone}"
+        zone_metric_name = f"count_zone_{zone}"
         latest_metrics[zone_metric_name] += 1
         
         return jsonify({"status": "success"}), 200
@@ -49,35 +44,37 @@ def dashboard():
 
 @app.route('/metrics', methods=['GET'])
 def metrics():
-    metrics_output = []
+    output = []
 
-    metrics_output.append("# TYPE angle_of_departure gauge")
-    metrics_output.append("# TYPE velocity_at_departure gauge")
-    metrics_output.append("# TYPE projected_yardage gauge")
-    metrics_output.append("# TYPE projected_zone gauge")
-    metrics_output.append("# TYPE total_balls_hit counter")
-    metrics_output.append("# TYPE count_zone_1 counter")
-    metrics_output.append("# TYPE count_zone_2 counter")
-    metrics_output.append("# TYPE count_zone_3 counter")
+    output.append("# TYPE angle_of_departure gauge")
+    output.append("# TYPE velocity_at_departure gauge")
+    output.append("# TYPE projected_yardage gauge")
+    output.append("# TYPE projected_zone gauge")
+    output.append("# TYPE total_balls_hit counter")
+    output.append("# TYPE count_zone_1 counter")
+    output.append("# TYPE count_zone_2 counter")
+    output.append("# TYPE count_zone_3 counter")
 
     for key, value in latest_metrics.items():
         if value is not None:
-            metrics_output.append(f"{key} {value}")
+            output.append(f"{key} {value}")
 
-    return '\n'.join(metrics_output), 200, {"Content-Type": "text/plain"}
+    return '\n'.join(output), 200, {"Content-Type": "text/plain"}
 
 
 
 @app.route('/clear-data', methods=['GET'])
 def clear_data():
-    latest_metrics["angle_of_departure"] = None
-    latest_metrics["velocity_at_departure"] = None
-    latest_metrics["projected_yardage"] = None
-    latest_metrics["projected_zone"] = None
-    latest_metrics["total_balls_hit"] = 0
-    latest_metrics["count_zone_1"] = 0
-    latest_metrics["count_zone_2"] = 0
-    latest_metrics["count_zone_3"] = 0
+    latest_metrics.update({
+        "angle_of_departure": None,
+        "velocity_at_departure": None,
+        "projected_yardage": None,
+        "projected_zone": None,
+        "total_balls_hit": 0,
+        "count_zone_1": 0,
+        "count_zone_2": 0,
+        "count_zone_3": 0
+    })
     return jsonify({"status": "success", "message": "All data has been cleared!"}), 200
 
 def calculate_projected_yardage(angle, velocity):
@@ -86,7 +83,7 @@ def calculate_projected_yardage(angle, velocity):
 def calculate_projected_zone(yardage):
     if yardage < 75:
         return 1
-    elif 75 <= yardage < 200:
+    elif yardage >= 75 and yardage < 200:
         return 2
     else:
         return 3
--- datadog/checks.d/swingsight_check.py
+++ datadog/checks.d/swingsight_check.py
@@ -4,16 +4,12 @@ import requests
 class SwingSightCheck(AgentCheck):
     def check(self, instance):
         try:
-            # Make a GET request to the Flask application's metrics endpoint
             response = requests.get('http://localhost:5050/metrics')
             response.raise_for_status()
             
-            # Parse the plain-text Prometheus-formatted metrics
             metrics_data = self._parse_metrics(response.text)
 
-            # Publish the metrics to Datadog
             for metric_name, value in metrics_data.items():
-                # Use the metric name as part of the Datadog metric name
                 datadog_metric_name = f'swingsight.{metric_name}'
                 self.gauge(datadog_metric_name, value)
         except Exception as e:
